d9d0704828d23d2252c822b77708f180a4a98c72
sikmi_watanabe <na8esin.mpst@gmail.com>
2024-07-17 07:01:59 +0900

    json bodyのrspec

--- rails71/spec/requests/api/samples_spec.rb
+++ rails71/spec/requests/api/samples_spec.rb
@@ -1,10 +1,17 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Api::Samples", type: :request do
-  describe "GET /api/samples" do
-    it "works! (now write some real specs)" do
-      get api_samples_path
+RSpec.describe 'Api::Samples', type: :request do
+  describe 'GET /api/samples' do
+    it 'works! (now write some real specs)' do
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      # paramsはhashじゃなくてJSON形式の文字列にする
+      post(api_sample_path, params: { page: 1 }.to_json, headers:)
       expect(response).to have_http_status(200)
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+      expect(response.body).to include({ page: '1', type: 'String' }.to_json)
     end
   end
 end
